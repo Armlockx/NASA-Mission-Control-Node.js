dbffbbabd1d3e0b8085e7dd8214194e2054c1ed8
Armlockx <reus_julio@hotmail.com>
2024-02-01 19:43:36 -0300

    Simple validation to launch properties implemented

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -11,7 +11,19 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
+    if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination ) {
+        return res.status(400).json({
+            error: 'Missing required launch property.',
+        });
+    }
+    
     launch.launchDate = new Date(launch.launchDate);
+    //if (launch.launchDate.toString() === 'Invalid Date'){
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid launch date inserted.',
+        });
+    }
 
     addNewLaunch(launch);
     return res.status(201).json(launch);    //201 = created
