7a991f3c2b1d34edb461a193fa0a3b259f712fcd
Armlockx <reus_julio@hotmail.com>
2024-02-19 12:10:04 -0300

    Jest fixes

---
+++ server/src/services/mongo.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = 'mongodb+srv://juliokillll2:M0SR23LTx39SQ5VC@nasacluster.paleuxs.mongodb.net/nasa?retryWrites=true&w=majority';
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect() {
+    await mongoose.disconnect();
+}
+
+module.exports = {
+    mongoConnect,
+    mongoDisconnect,
+}
